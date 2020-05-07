8c5719883364eb0b6053e84ee3b91c9a6db9c414
xiyu-526 <997578871@qq.com>
2020-05-07 12:29:13 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <ul>
+      <li v-for="item in user" :key="item.id">
+        {{ item.id }}： {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      user: []
+    }
+  },
+  computed: {
+
+  },
+  mounted() {
+    axios.get('http://192.168.140.178:3000/api').then((resp) => {
+      this.user = resp.data;
+    }, (err) => {
+      alert(err);
+    })
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const publicPathMap = {
+  pre: '/act/vue-web/',
+  production: '/act/vue-web/',
+  development: '/'
+};
+
+module.exports = {
+  publicPath: publicPathMap[process.env.VUE_APP_ENV],
+  outputDir: process.env.VUE_APP_ENV == 'pre' ? 'pre' : 'dist',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, 'src'),
+      },
+    },
+  },
+  chainWebpack: config => {
+    config
+      .output
+      .filename('js/[name].[hash:3].js')
+      .end();
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = './src/index.html'
+        return args
+      })
+  },
+  devServer: {
+    proxy: {
+      '/api/*': {
+        target: 'http://192.168.140.56:3000/',
+        changeOrigin: 'true'
+      }
+    },
+  }
+}
